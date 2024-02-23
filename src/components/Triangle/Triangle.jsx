0b6f2d594c5cb938d8129ac76ecada3651675ff3
Victória Lima <victarialima@gmail.com>
2024-02-23 16:31:53 +0000

    Update Triangle.jsx

--- src/components/Triangle/Triangle.jsx
+++ src/components/Triangle/Triangle.jsx
@@ -4,7 +4,7 @@ export function Triangle() {
 
     return(
         <div>
-            <h1>Forma Geométrica: Triangulo</h1>
+            <h1>Forma Geométrica: Triângulo</h1>
             <h2>Quantidades de Lados: 3</h2>
             <div className={styles.triangle}></div>
             
